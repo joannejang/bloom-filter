c293f522212f777cc21a81f66bd1e6e7a327cb1f
Joanne Jang <jang.joanne@gmail.com>
2016-12-28 03:08:22 -0800

    bug in accessing bits[sfh/NUM_BITS] and so forth

--- bloom.c
+++ bloom.c
@@ -13,20 +13,20 @@ bloom_filter* create_filter(size_t size) {
 
 void add_entry(bloom_filter *filter, const void *entry) {
 	assert(entry);
-	const char *entry_str = *(char **) entry;
+	const char *entry_str = (const char *) entry;
 	int length = strlen(entry_str);
 	uint32_t sfh = hash_SuperFastHash(entry_str, length);
   uint32_t djb = hash_djb2(entry_str);
   uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length);
-  (*(char **) filter->bits)[sfh/NUM_BITS] |= 1 << sfh%NUM_BITS;
-  (*(char **) filter->bits)[djb/NUM_BITS] |= 1 << djb%NUM_BITS;
-  (*(char **) filter->bits)[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS;
+  ((char *) filter->bits)[sfh/NUM_BITS] |= 1 << sfh%NUM_BITS;
+  ((char *) filter->bits)[djb/NUM_BITS] |= 1 << djb%NUM_BITS;
+  ((char *) filter->bits)[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS;
 }
 
 bool possibly_present(bloom_filter *filter, const void *entry) {
   assert(entry);
-  const char *entry_str = *(char **) entry;
-  char *bits = *(char **) filter->bits;
+  const char *entry_str = (const char *) entry;
+  char *bits = (char *) filter->bits;
   int length = strlen(entry_str);
   uint32_t sfh = hash_SuperFastHash(entry_str, length);
   uint32_t djb = hash_djb2(entry_str);
