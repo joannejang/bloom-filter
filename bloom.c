6bf17bf251dee3976e7864ca4d0375c5800a4fa0
Joanne Jungwon Jang <joanne.jang@stanford.edu>
2016-12-28 09:04:13 -0800

    math might be off

--- bloom.c
+++ bloom.c
@@ -15,12 +15,12 @@ void add_entry(bloom_filter *filter, const void *entry) {
 	assert(entry);
 	const char *entry_str = (const char *) entry;
 	int length = strlen(entry_str);
-	uint32_t sfh = hash_SuperFastHash(entry_str, length);
-  uint32_t djb = hash_djb2(entry_str);
-  uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length);
-  ((char *) filter->bits)[sfh/NUM_BITS] |= 1 << sfh%NUM_BITS;
-  ((char *) filter->bits)[djb/NUM_BITS] |= 1 << djb%NUM_BITS;
-  ((char *) filter->bits)[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS;
+	uint32_t sfh = hash_SuperFastHash(entry_str, length) % NUM_BITS;
+  uint32_t djb = hash_djb2(entry_str) % NUM_BITS;
+  uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length) % NUM_BITS;
+  ((char *) filter->bits)[sfh/NUM_BITS] |= 1 << sfh;
+  ((char *) filter->bits)[djb/NUM_BITS] |= 1 << djb;
+  ((char *) filter->bits)[jenk/NUM_BITS] |= 1 << jenk;
 }
 
 bool possibly_present(bloom_filter *filter, const void *entry) {
@@ -28,13 +28,13 @@ bool possibly_present(bloom_filter *filter, const void *entry) {
   const char *entry_str = (const char *) entry;
   char *bits = (char *) filter->bits;
   int length = strlen(entry_str);
-  uint32_t sfh = hash_SuperFastHash(entry_str, length);
-  uint32_t djb = hash_djb2(entry_str);
-  uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length);
+  uint32_t sfh = hash_SuperFastHash(entry_str, length) % NUM_BITS;
+  uint32_t djb = hash_djb2(entry_str) % NUM_BITS;
+  uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length) % NUM_BITS;
   
-  return ((bits[sfh/NUM_BITS] & 1 << sfh%NUM_BITS) &&
-  (bits[djb/NUM_BITS] |= 1 << djb%NUM_BITS) &&
-  (bits[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS));
+  return ((bits[sfh/NUM_BITS] & 1 << sfh) &&
+  (bits[djb/NUM_BITS] |= 1 << djb) &&
+  (bits[jenk/NUM_BITS] |= 1 << jenk));
 }
 
 void free_filter(bloom_filter *filter) {
