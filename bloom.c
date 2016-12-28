b92dd64c4b876489d02767c63713f66d7ae64936
Joanne Jang <jang.joanne@gmail.com>
2016-12-28 09:05:58 -0800

    changing unsigned char * to uint32_t *

--- bloom.c
+++ bloom.c
@@ -18,15 +18,15 @@ void add_entry(bloom_filter *filter, const void *entry) {
 	uint32_t sfh = hash_SuperFastHash(entry_str, length);
   uint32_t djb = hash_djb2(entry_str);
   uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length);
-  ((char *) filter->bits)[sfh/NUM_BITS] |= 1 << sfh%NUM_BITS;
-  ((char *) filter->bits)[djb/NUM_BITS] |= 1 << djb%NUM_BITS;
-  ((char *) filter->bits)[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS;
+  ((uint32_t *) filter->bits)[sfh/NUM_BITS] |= 1 << sfh%NUM_BITS;
+  ((uint32_t *) filter->bits)[djb/NUM_BITS] |= 1 << djb%NUM_BITS;
+  ((uint32_t *) filter->bits)[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS;
 }
 
 bool possibly_present(bloom_filter *filter, const void *entry) {
   assert(entry);
   const char *entry_str = (const char *) entry;
-  char *bits = (char *) filter->bits;
+  uint32_t *bits = (uint32_t *) filter->bits;
   int length = strlen(entry_str);
   uint32_t sfh = hash_SuperFastHash(entry_str, length);
   uint32_t djb = hash_djb2(entry_str);
