4de1a05135eb1e2e3ebf1a36963fb3d0db4e98b3
Joanne Jungwon Jang <joanne.jang@stanford.edu>
2016-12-28 02:10:20 -0800

    just wrote a simple test and segfaults, will look into it tomorrow

--- bloom.c
+++ bloom.c
@@ -24,7 +24,17 @@ void add_entry(bloom_filter *filter, const void *entry) {
 }
 
 bool possibly_present(bloom_filter *filter, const void *entry) {
-	return false;
+  assert(entry);
+  const char *entry_str = *(char **) entry;
+  char *bits = *(char **) filter->bits;
+  int length = strlen(entry_str);
+  uint32_t sfh = hash_SuperFastHash(entry_str, length);
+  uint32_t djb = hash_djb2(entry_str);
+  uint32_t jenk = hash_jenkins_one_at_a_time(entry_str, length);
+  
+  return ((bits[sfh/NUM_BITS] & 1 << sfh%NUM_BITS) &&
+  (bits[djb/NUM_BITS] |= 1 << djb%NUM_BITS) &&
+  (bits[jenk/NUM_BITS] |= 1 << jenk%NUM_BITS));
 }
 
 void free_filter(bloom_filter *filter) {
--- tests.c
+++ tests.c
@@ -5,7 +5,15 @@
 
 int main() {
 	bloom_filter* filter = create_filter(INIT_SIZE);
-	assert(!filter);
-	printf("yay got a null ptr back <3\n");
+	assert(filter);
+	
+	printf("Should be false: ");
+	printf(possibly_present(filter, "apple") ? "true\n" : "false\n");
+
+	add_entry(filter, "apple");
+	printf("Could be true (but false is okay too): ");
+	printf(possibly_present(filter, "apple") ? "true\n" : "false\n");
+
+	free_filter(filter);
 	return 0;
 }
